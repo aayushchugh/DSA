7a29ff2b4df5e9ca6598e34ef326c31ccc3afb01
Ayush Chugh <ayushchugh2006@gmail.com>
2024-02-19 17:44:44 +0530

    bubble sort

---
+++ sorting/bubbleSort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  int arr[] = {5, 2, 3, 1, 4};
+
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = i + 1; j < 5; j++)
+    {
+      if (arr[i] > arr[j])
+      {
+       int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  
+  printf("Sorted array: \n");
+  for (int i = 0; i < 5; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+
+  return 0;
+}
